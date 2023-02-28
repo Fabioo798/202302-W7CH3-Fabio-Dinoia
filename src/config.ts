cd06e4ca0b2312de4284457c2b1ac31ef58be9ee
Fabio <fabiodn.798@gmail.com>
2023-02-28 18:18:42 +0100

    🔨 Create db config

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const config = {
+  user: process.env.USERNAME,
+  pass: process.env.PASSWORD,
+  cluster: process.env.CLUSTER,
+  dbname: process.env.NAME,
+};
+
