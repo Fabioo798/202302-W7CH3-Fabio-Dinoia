5f423354d5edf80164bfb0ccd53e78741375ff23
Fabio <fabiodn.798@gmail.com>
2023-02-28 18:17:32 +0100

    🚧 Create index

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import http from 'http';
+import createDebug from 'debug';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
+import mongoose from 'mongoose';
+
+const debug = createDebug('W7CH3: index');
+const PORT = process.env.port || 4000;
+
+const server = http.createServer(app);
+
+dbConnect()
+.then((mongoose) => {
+  server.listen(PORT);
+  debug('DB', mongoose.connection.db.databaseName);
+})
+.catch((error) => server.emit('error', error));
+
+server.on('error', (error) => {
+  debug('DB: ', error.message);
+});
+server.on('listening', () => {
+  debug('http://localhost:' + PORT);
+})
